7b271da118c3b25f43ed932e87f8613c94b5691e
Ramata N'diaye <ramatand23@gmail.com>
2021-08-04 09:08:05 -0600

    Tab Bar update

---
+++ TDDSnipApp/Views/TabBarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarViewController.swift
+//  TDDSnipApp
+//
+//  Created by Ramata N’diaye on 8/4/21.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    @IBOutlet weak var home: UITabBarItem!
+    
+    @IBOutlet weak var account: UITabBarItem!
+    
+    @IBOutlet weak var share: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
